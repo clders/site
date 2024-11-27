94f3bea0cee6a5fae11b84456db65394b7bd539a
Tony Kan <tianchengkan@gmail.com>
2024-11-27 14:34:16 -0800

    chore(project): Setup Next.js, Tailwind CSS, ESLint, and Prettier
    - Installed necessary packages.
    - Configured Next.js.
    - Configured Tailwind CSS.
    - Configured ESLint.
    - Configured Prettier.

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'double'],
+      curly: ['error', 'multi-line'],
+      eqeqeq: ['error', 'always'],
+      'no-duplicate-imports': 'error',
+      'no-magic-numbers': ['error', { ignore: [0, 1] }],
+      'consistent-return': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-unused-vars': 'warn',
+      'no-shadow': 'error',
+      'func-names': 'error',
+      'prefer-arrow-callback': 'error',
+    },
+  },
+];
